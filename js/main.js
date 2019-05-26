05f1de4e1c8bed909c2091b4061836447ef18e61
zhangyanlei <zhangyanlei@it168.com>
2019-05-27 01:02:06 +0800

    ff

---
+++ js/main.js
@@ -0,0 +1,69 @@
+//轮播图
+function slider(){
+    var mySwiper = new Swiper ('.slide-box-top', {
+        loop: true,
+        autoplay : 3000,
+        autoplayDisableOnInteraction : false,
+        pagination: '.pagination',
+        paginationClickable: true,
+        centeredSlides: true,
+        slidesPerView: 3,
+        watchActiveIndex: true
+    });
+    var mySwiper = new Swiper ('.slide-box-new', {
+        loop: true,
+        autoplayDisableOnInteraction : false,
+        centeredSlides: true,
+        slidesPerView: 3,
+        watchActiveIndex: true
+    });
+    $(".slide-box-new").mouseover(function () {
+        $(".swiper-btn",this).show();
+        mySwiper.stopAutoplay();
+    }).mouseout(function () {
+        // $(".swiper-btn",this).hide();
+        mySwiper.startAutoplay();
+    });
+    $('.new-box .btnLeft').on('click', function(e){
+        e.preventDefault()
+        mySwiper.stopAutoplay();
+        mySwiper.swipePrev();
+        mySwiper.startAutoplay();
+    })
+    $('.new-box .btnRight').on('click', function(e){
+        e.preventDefault()
+        mySwiper.stopAutoplay();
+        mySwiper.swipeNext();
+        mySwiper.startAutoplay();
+    })
+}
+
+function isShowToolbar(){
+        $(window).scroll(function(){
+            if($(document).scrollTop()>=50){
+                $(".toolbar").show();
+            }else {
+                $(".toolbar").hide();
+            }
+        });
+        $(".to-top").on("click",function () {
+            $("html").animate({"scrollTop":0},300);
+        });
+}
+
+function navAnim(){
+    $(".nav-list").on("mouseover",function () {
+        var height = $(this).find(".fix-height").height();
+        $(this).find(".child-box").height(height);
+    }).on("mouseout",function () {
+        $(this).find(".child-box").height(0);
+    });
+}
+
+
+
+$(function () {
+    navAnim();
+    slider();
+    isShowToolbar();
+})
